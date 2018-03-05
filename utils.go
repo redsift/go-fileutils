4f25360ab5d5caa7cdaab08445a4dc9f960e572d
Christos Vontas <christos@redsift.io>
2018-03-05 19:25:02 +0000

    added AtomicWrite

---
+++ utils.go
@@ -0,0 +1,34 @@
+package go_fileutils
+
+import (
+	"os"
+	"io/ioutil"
+	"path/filepath"
+)
+
+func AtomicWrite(filename string, data []byte, perm os.FileMode) error {
+	tname, err := func() (string, error){
+		f, err := ioutil.TempFile(filepath.Dir(filename), "._")
+		if err != nil {
+			return "", err
+		}
+		defer f.Close()
+
+		_, err = f.Write(data)
+		if err != nil {
+			return "", err
+		}
+		return f.Name(), nil
+	}()
+
+	if err != nil {
+		return err
+	}
+
+	err = os.Rename(tname, filename)
+	if err != nil {
+		return err
+	}
+
+	return os.Chmod(filename, perm)
+}
